8ac998ee9f27ec4326ae7138f0f515d38975b643
Milind Mantri <milind.mantri@loconav.com>
2025-01-09 03:05:29 +0530

    Set url crawling scope strategy to limit domains

--- src/main/java/com/milindmantri/Main.java
+++ src/main/java/com/milindmantri/Main.java
@@ -3,6 +3,7 @@ package com.milindmantri;
 import com.norconex.collector.http.HttpCollector;
 import com.norconex.collector.http.HttpCollectorConfig;
 import com.norconex.collector.http.crawler.HttpCrawlerConfig;
+import com.norconex.collector.http.crawler.URLCrawlScopeStrategy;
 
 public class Main {
 
@@ -19,6 +20,13 @@ public class Main {
     // TODO: pass list of all URLs to crawl
     crawlerConfig.setStartURLs("https://www.php.net");
     crawlerConfig.setId("test-crawler");
+
+    // TODO: What about the case for redirected domains, like openjdk.java.net
+    var urlCrawlScope = new URLCrawlScopeStrategy();
+    urlCrawlScope.setStayOnDomain(true);
+    urlCrawlScope.setIncludeSubdomains(false);
+
+    crawlerConfig.setUrlCrawlScopeStrategy(urlCrawlScope);
 
     config.setCrawlerConfigs(crawlerConfig);
 
