1be52bc14656ca79a257f1f7372929f19d031839
Milind Mantri <milind.mantri@loconav.com>
2025-01-09 03:00:36 +0530

    Setup crawler - trial

--- src/main/java/com/milindmantri/Main.java
+++ src/main/java/com/milindmantri/Main.java
@@ -1,8 +1,29 @@
 package com.milindmantri;
 
+import com.norconex.collector.http.HttpCollector;
+import com.norconex.collector.http.HttpCollectorConfig;
+import com.norconex.collector.http.crawler.HttpCrawlerConfig;
+
 public class Main {
 
   public static void main(String[] args) {
 
+    // Why even allow for new HttpCollector(), when setting id is required. It will anyway error.
+    HttpCollectorConfig config = new HttpCollectorConfig();
+    config.setId("test");
+
+    config.setMaxConcurrentCrawlers(2);
+
+    HttpCrawlerConfig crawlerConfig = new HttpCrawlerConfig();
+
+    // TODO: pass list of all URLs to crawl
+    crawlerConfig.setStartURLs("https://www.php.net");
+    crawlerConfig.setId("test-crawler");
+
+    config.setCrawlerConfigs(crawlerConfig);
+
+    HttpCollector spider = new HttpCollector(config);
+
+    spider.start();
   }
 }
