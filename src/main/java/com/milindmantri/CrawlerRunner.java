5b504f427aec33f2c59272aa45c849587f834175
Milind Mantri <milind.mantri@loconav.com>
2025-02-10 21:14:20 +0530

    Schedule crawling

---
+++ src/main/java/com/milindmantri/CrawlerRunner.java
@@ -0,0 +1,103 @@
+package com.milindmantri;
+
+import com.norconex.collector.core.filter.IMetadataFilter;
+import com.norconex.collector.core.filter.impl.MetadataFilter;
+import com.norconex.collector.http.HttpCollector;
+import com.norconex.collector.http.HttpCollectorConfig;
+import com.norconex.collector.http.crawler.HttpCrawlerConfig;
+import com.norconex.collector.http.crawler.URLCrawlScopeStrategy;
+import com.norconex.collector.http.url.impl.GenericURLNormalizer;
+import com.norconex.commons.lang.text.TextMatcher;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.sql.SQLException;
+import java.util.stream.Stream;
+import javax.sql.DataSource;
+
+public final class CrawlerRunner implements Runnable {
+
+  private static final int PER_HOST_CRAWLING_LIMIT = 10_000;
+
+  private final DataSource datasource;
+
+  public CrawlerRunner(final DataSource datasource) {
+    if (datasource == null) {
+      throw new IllegalArgumentException("datasource must not be null.");
+    }
+
+    this.datasource = datasource;
+  }
+
+  @Override
+  public void run() {
+    // Why even allow for new HttpCollector(), when setting id is required. It will anyway error.
+    HttpCollectorConfig config = new HttpCollectorConfig();
+    config.setId(System.getProperty("collector-id", "ps"));
+
+    HttpCrawlerConfig crawlerConfig = new HttpCrawlerConfig();
+
+    // TODO: Delete orphan URLs and spoiled refs
+
+    crawlerConfig.setUrlNormalizer(new GenericURLNormalizer());
+
+    // TODO: pass list of all URLs to crawl
+    crawlerConfig.setStartURLs("https://www.php.net", "https://elm-lang.org");
+
+    // TODO: Set threads for crawler
+
+    crawlerConfig.setId(System.getProperty("crawler-id", "crwlr"));
+
+    // TODO: What about the case for redirected domains, like openjdk.java.net
+    var urlCrawlScope = new URLCrawlScopeStrategy();
+    urlCrawlScope.setStayOnDomain(true);
+    urlCrawlScope.setIncludeSubdomains(false);
+    crawlerConfig.setUrlCrawlScopeStrategy(urlCrawlScope);
+
+    crawlerConfig.setIgnoreCanonicalLinks(true);
+    crawlerConfig.setFetchHttpHead(true);
+    crawlerConfig.setMetadataFilters(getTextOnlyMetadataFilters().toList());
+
+    try (ProsearchJdbcDataStoreEngine engine = new ProsearchJdbcDataStoreEngine()) {
+
+      engine.setConfigProperties(Main.dbProps());
+      crawlerConfig.setDataStoreEngine(engine);
+
+      var domainCounter = new DomainCounter(PER_HOST_CRAWLING_LIMIT, this.datasource);
+      crawlerConfig.setEventListeners(domainCounter);
+      crawlerConfig.setReferenceFilters(domainCounter);
+
+      crawlerConfig.setCommitters(
+          new TantivyCommitter(
+              new TantivyClient(
+                  HttpClient.newBuilder()
+                      .followRedirects(HttpClient.Redirect.NORMAL)
+                      .version(HttpClient.Version.HTTP_1_1)
+                      .build(),
+                  URI.create(System.getProperty("tantivy-server", "http://localhost:3000"))),
+              this.datasource));
+
+      config.setCrawlerConfigs(crawlerConfig);
+
+      HttpCollector spider = new HttpCollector(config);
+
+      // TODO: Remove once testing is complete
+      spider.clean();
+
+      spider.start();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static Stream<IMetadataFilter> getTextOnlyMetadataFilters() {
+    return Stream.of("text/html", "application/xhtml+xml", "text/plain")
+        .map(t -> t + "*")
+        .mapMulti(
+            (t, c) -> {
+              c.accept(
+                  new MetadataFilter(TextMatcher.basic("Content-Type"), TextMatcher.wildcard(t)));
+              c.accept(
+                  new MetadataFilter(TextMatcher.basic("content-type"), TextMatcher.wildcard(t)));
+            });
+  }
+}
--- src/main/java/com/milindmantri/Main.java
+++ src/main/java/com/milindmantri/Main.java
@@ -1,26 +1,18 @@
 package com.milindmantri;
 
-import com.norconex.collector.core.filter.IMetadataFilter;
-import com.norconex.collector.core.filter.impl.MetadataFilter;
-import com.norconex.collector.http.HttpCollector;
-import com.norconex.collector.http.HttpCollectorConfig;
-import com.norconex.collector.http.crawler.HttpCrawlerConfig;
-import com.norconex.collector.http.crawler.URLCrawlScopeStrategy;
-import com.norconex.collector.http.url.impl.GenericURLNormalizer;
-import com.norconex.commons.lang.text.TextMatcher;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import java.net.URI;
-import java.net.http.HttpClient;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 import javax.sql.DataSource;
 
 public class Main {
-
-  private static final int PER_HOST_CRAWLING_LIMIT = 10_000;
 
   private static final String CREATE_DOMAIN_STATS_TABLE =
       """
@@ -42,70 +34,24 @@ public class Main {
       )
       """;
 
-  public static void main(String[] args) throws SQLException {
+  public static void main(String[] args)
+      throws SQLException, ExecutionException, InterruptedException {
 
-    // Why even allow for new HttpCollector(), when setting id is required. It will anyway error.
-    HttpCollectorConfig config = new HttpCollectorConfig();
-    config.setId(System.getProperty("collector-id", "ps"));
-
-    // TODO: crawler automatically closes after finish. We want to recrawl routinely.
-
-    HttpCrawlerConfig crawlerConfig = new HttpCrawlerConfig();
-
-    // TODO: Delete orphan URLs and spoiled refs
-
-    crawlerConfig.setUrlNormalizer(new GenericURLNormalizer());
-
-    // TODO: pass list of all URLs to crawl
-    crawlerConfig.setStartURLs("https://www.php.net", "https://elm-lang.org");
-
-    // TODO: Set threads for crawler
-
-    crawlerConfig.setId(System.getProperty("crawler-id", "crwlr"));
-
-    // TODO: What about the case for redirected domains, like openjdk.java.net
-    var urlCrawlScope = new URLCrawlScopeStrategy();
-    urlCrawlScope.setStayOnDomain(true);
-    urlCrawlScope.setIncludeSubdomains(false);
-    crawlerConfig.setUrlCrawlScopeStrategy(urlCrawlScope);
-
-    crawlerConfig.setIgnoreCanonicalLinks(true);
-    crawlerConfig.setFetchHttpHead(true);
-    crawlerConfig.setMetadataFilters(getTextOnlyMetadataFilters().toList());
-
-    try (ProsearchJdbcDataStoreEngine engine = new ProsearchJdbcDataStoreEngine();
-        var dataSource = new HikariDataSource(new HikariConfig(dbProps().toProperties()))) {
+    try (var dataSource = new HikariDataSource(new HikariConfig(dbProps().toProperties()));
+        ScheduledExecutorService crawlerScheduler = Executors.newSingleThreadScheduledExecutor()) {
 
       // TODO: Remove once testing is complete
       dropStatsTable(dataSource);
 
       createStatsTableIfNotExists(dataSource);
 
-      engine.setConfigProperties(dbProps());
-      crawlerConfig.setDataStoreEngine(engine);
+      int delayBetweenRuns =
+          Integer.parseInt(System.getProperty("delay-hours-between-crawls", "4"));
+      ScheduledFuture<?> sf =
+          crawlerScheduler.scheduleWithFixedDelay(
+              new CrawlerRunner(dataSource), 0, delayBetweenRuns, TimeUnit.HOURS);
 
-      var domainCounter = new DomainCounter(PER_HOST_CRAWLING_LIMIT, dataSource);
-      crawlerConfig.setEventListeners(domainCounter);
-      crawlerConfig.setReferenceFilters(domainCounter);
-
-      crawlerConfig.setCommitters(
-          new TantivyCommitter(
-              new TantivyClient(
-                  HttpClient.newBuilder()
-                      .followRedirects(HttpClient.Redirect.NORMAL)
-                      .version(HttpClient.Version.HTTP_1_1)
-                      .build(),
-                  URI.create(System.getProperty("tantivy-server", "http://localhost:3000"))),
-              dataSource));
-
-      config.setCrawlerConfigs(crawlerConfig);
-
-      HttpCollector spider = new HttpCollector(config);
-
-      // TODO: Remove once testing is complete
-      spider.clean();
-
-      spider.start();
+      sf.get();
     }
   }
 
@@ -129,24 +75,12 @@ public class Main {
     }
   }
 
-  private static com.norconex.commons.lang.map.Properties dbProps() {
+  static com.norconex.commons.lang.map.Properties dbProps() {
     var props = new HashMap<String, List<String>>();
     props.put("jdbcUrl", List.of("jdbc:postgresql://localhost:5432/milind"));
     props.put("username", List.of("postgres"));
     props.put("password", List.of("pass"));
 
     return new com.norconex.commons.lang.map.Properties(props);
-  }
-
-  private static Stream<IMetadataFilter> getTextOnlyMetadataFilters() {
-    return Stream.of("text/html", "application/xhtml+xml", "text/plain")
-        .map(t -> t + "*")
-        .mapMulti(
-            (t, c) -> {
-              c.accept(
-                  new MetadataFilter(TextMatcher.basic("Content-Type"), TextMatcher.wildcard(t)));
-              c.accept(
-                  new MetadataFilter(TextMatcher.basic("content-type"), TextMatcher.wildcard(t)));
-            });
   }
 }
